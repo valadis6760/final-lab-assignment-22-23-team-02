6a70d83a2f86c91054637921f4a653bfb2166510
Juan Astorga Diz <juan.adiz@alumnos.upm.es>
2023-05-01 18:24:55 +0200

    Update config-env.sh

--- config-env.sh
+++ config-env.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# export DRONE_APP_PATH=/home/drone-app
+export DRONE_APP_PATH=/home/drone-app
 
-# sudo apt -y install python3 python3-pip
-# pip3 install -r requirements.txt
+sudo apt -y install python3 python3-pip
+pip3 install -r requirements.txt
 
-# if [ -d ${DRONE_APP_PATH} ]; then
-#     echo "The directory is already created"
-# else
-#     mkdir ${DRONE_APP_PATH}
-# fi
+if [ -d ${DRONE_APP_PATH} ]; then
+    echo "The directory is already created"
+else
+    mkdir ${DRONE_APP_PATH}
+fi
 
-# git clone https://github.com/ETSISI-CCforIoT/final-lab-assignment-22-23-team-02.git
+cd ${DRONE_APP_PATH}
+git clone https://github.com/ETSISI-CCforIoT/final-lab-assignment-22-23-team-02.git
+cd final-lab-assignment-22-23-team-02
 
-# # mkdir ${DRONE_APP_PATH}
-# sudo cp -r . ${DRONE_APP_PATH}
-
-ls
+sudo cp -r . ${DRONE_APP_PATH}
