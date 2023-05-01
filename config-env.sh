5643552eb513ca113159a5a8a4c43ff3dec0177a
Juan Astorga Diz <juan.adiz@alumnos.upm.es>
2023-05-01 17:42:33 +0200

    Update config-env.sh

--- config-env.sh
+++ config-env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo ${HOME}
-# sudo apt -y install python3 python3-pip
-# pip3 install -r requirements.txt
-# sudo cp -r . ${HOME}/drone-src
+export DRONE_APP_PATH=/home/drone-app
+
+sudo apt -y install python3 python3-pip
+pip3 install -r requirements.txt
+sudo cp -r . ${DRONE_APP_PATH}/drone-src
