d22d9f32890f6acc0297b0c76e7d97014645ef28
Juan Astorga Diz <juan.adiz@alumnos.upm.es>
2023-05-01 17:36:55 +0200

    test echo

--- config-env.sh
+++ config-env.sh
@@ -1,3 +1,6 @@
-sudo apt -y install python3 python3-pip
-pip3 install -r requirements.txt
-sudo cp -r . ${HOME}/drone-src
+#!/bin/bash
+
+echo ${HOME}
+# sudo apt -y install python3 python3-pip
+# pip3 install -r requirements.txt
+# sudo cp -r . ${HOME}/drone-src
